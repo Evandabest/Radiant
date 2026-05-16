ca53623e497ad9a883a5de9d40193c24d5f24e8a
Evandabest <evanhaque1@gmail.com>
2026-05-16 11:57:39 -0400

    Set activation policy to accessory so app doesn't participate in app switching

--- Radiant/Radiant/RadiantApp.swift
+++ Radiant/Radiant/RadiantApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.accessory)
+    }
+}
+
 @main
 struct RadiantApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @State private var controller = BrightnessController()
 
     var body: some Scene {
