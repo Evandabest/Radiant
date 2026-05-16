47437eb75477b7a49b61f7233985361c3449d596
Evandabest <evanhaque1@gmail.com>
2026-05-16 04:06:02 -0400

    Add EDRMetalView with HDR clear color rendering

---
+++ Radiant/Radiant/Core/EDRMetalView.swift
@@ -0,0 +1,49 @@
+import AppKit
+import MetalKit
+
+class EDRMetalView: MTKView, MTKViewDelegate {
+    private var commandQueue: MTLCommandQueue?
+
+    var boostFactor: Double = 1.0 {
+        didSet {
+            clearColor = MTLClearColor(red: boostFactor, green: boostFactor, blue: boostFactor, alpha: 1.0)
+        }
+    }
+
+    init(frame: CGRect) {
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            fatalError("Metal is not supported on this device")
+        }
+        super.init(frame: frame, device: device)
+
+        commandQueue = device.makeCommandQueue()
+        delegate = self
+
+        colorPixelFormat = .rgba16Float
+        colorspace = CGColorSpace(name: CGColorSpace.extendedLinearDisplayP3)
+        clearColor = MTLClearColor(red: boostFactor, green: boostFactor, blue: boostFactor, alpha: 1.0)
+        preferredFramesPerSecond = 10
+
+        if let metalLayer = layer as? CAMetalLayer {
+            metalLayer.wantsExtendedDynamicRangeContent = true
+        }
+    }
+
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+
+    func draw(in view: MTKView) {
+        guard let drawable = currentDrawable,
+              let descriptor = currentRenderPassDescriptor,
+              let commandBuffer = commandQueue?.makeCommandBuffer() else { return }
+
+        let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor)
+        encoder?.endEncoding()
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}
