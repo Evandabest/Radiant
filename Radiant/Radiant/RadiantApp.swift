4979d5fb5b7f61fcb49e93dcb7e39c93ac470755
Evandabest <evanhaque1@gmail.com>
2026-05-16 04:08:24 -0400

    Convert app to MenuBarExtra with brightness boost toggle

--- Radiant/Radiant/RadiantApp.swift
+++ Radiant/Radiant/RadiantApp.swift
@@ -1,17 +1,28 @@
-//
-//  RadiantApp.swift
-//  Radiant
-//
-//  Created by Evan Haque on 5/16/26.
-//
-
 import SwiftUI
 
 @main
 struct RadiantApp: App {
+    @State private var overlayManager = BrightnessOverlayManager()
+    @State private var isEnabled = false
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Radiant", systemImage: isEnabled ? "sun.max.fill" : "sun.max") {
+            Toggle("Boost Brightness", isOn: $isEnabled)
+                .onChange(of: isEnabled) { _, newValue in
+                    if newValue {
+                        if let screen = NSScreen.main {
+                            overlayManager.boostFactor = 2.0
+                            overlayManager.activate(on: screen)
+                        }
+                    } else {
+                        overlayManager.deactivate()
+                    }
+                }
+            Divider()
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
+            .keyboardShortcut("q")
         }
     }
 }
