d03d48922c0c1a15c81eba5195e7ef3ffb7139a4
Evandabest <evanhaque1@gmail.com>
2026-05-16 04:04:18 -0400

    Initial Xcode project setup

---
+++ Radiant/Radiant/RadiantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadiantApp.swift
+//  Radiant
+//
+//  Created by Evan Haque on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct RadiantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
